13bd57bef1ed7a719a45951eba8094edda10fa22
xaatim <xayari229@gmail.com>
2025-12-21 08:16:36 +0800

    changes

--- platform/scuttle/ROS2/build/generic_robot_driver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/hatim/Documents/Github/robofun_ws/platform/scuttle/ROS2/install/generic_robot_driver'
